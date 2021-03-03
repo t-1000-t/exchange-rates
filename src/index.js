f2c8ae26c3b767b62e63ce37e8bb1750af1ef54b
t-1000-t <goognya@gmail.com>
2021-03-03 09:14:42 +0200

    calc

--- src/index.js
+++ src/index.js
@@ -48,38 +48,38 @@ function calculate() {
       case "EUR":
         let rateE = data.find(elem => elem.ccy === "EUR")
         let totalE = Number(inputForm.value) * Number(rateE.sale)
-        Li11.firstElementChild.textContent = totalE.toFixed(2) + ' грн.'
-        Li12.firstElementChild.textContent = (totalE / 1.2).toFixed(2) + ' грн.'
-        Li13.firstElementChild.textContent = ((totalE / 1.2) * 1.05).toFixed(2) + ' грн.'
-        Li14.firstElementChild.textContent = ((totalE / 1.2) * 1.1).toFixed(2) + ' грн.'
-        Li15.firstElementChild.textContent = ((totalE / 1.2) * 1.15).toFixed(2) + ' грн.'
-        Li16.firstElementChild.textContent = ((totalE / 1.2) * 1.2).toFixed(2) + ' грн.'
-        Li17.firstElementChild.textContent = ((totalE / 1.2) * 1.25).toFixed(2) + ' грн.'
-        Li18.firstElementChild.textContent = ((totalE / 1.2) * 1.3).toFixed(2) + ' грн.'
+        Li11.lastElementChild.textContent = totalE.toFixed(2) + ' грн.'
+        Li12.lastElementChild.textContent = (totalE / 1.2).toFixed(2) + ' грн.'
+        Li13.lastElementChild.textContent = ((totalE / 1.2) * 1.05).toFixed(2) + ' грн.'
+        Li14.lastElementChild.textContent = ((totalE / 1.2) * 1.1).toFixed(2) + ' грн.'
+        Li15.lastElementChild.textContent = ((totalE / 1.2) * 1.15).toFixed(2) + ' грн.'
+        Li16.lastElementChild.textContent = ((totalE / 1.2) * 1.2).toFixed(2) + ' грн.'
+        Li17.lastElementChild.textContent = ((totalE / 1.2) * 1.25).toFixed(2) + ' грн.'
+        Li18.lastElementChild.textContent = ((totalE / 1.2) * 1.3).toFixed(2) + ' грн.'
         break
       case "RUR":
         let rateR = data.find(elem => elem.ccy === "RUR")
         let totalR = Number(inputForm.value) * Number(rateR.sale)
-        Li11.firstElementChild.textContent = totalR.toFixed(2) + ' грн.'
-        Li12.firstElementChild.textContent = (totalR / 1.2).toFixed(2) + ' грн.'
-        Li13.firstElementChild.textContent = ((totalR / 1.2) * 1.05).toFixed(2) + ' грн.'
-        Li14.firstElementChild.textContent = ((totalR / 1.2) * 1.1).toFixed(2) + ' грн.'
-        Li15.firstElementChild.textContent = ((totalR / 1.2) * 1.15).toFixed(2) + ' грн.'
-        Li16.firstElementChild.textContent = ((totalR / 1.2) * 1.2).toFixed(2) + ' грн.'
-        Li17.firstElementChild.textContent = ((totalR / 1.2) * 1.25).toFixed(2) + ' грн.'
-        Li18.firstElementChild.textContent = ((totalR / 1.2) * 1.3).toFixed(2) + ' грн.'
+        Li11.lastElementChild.textContent = totalR.toFixed(2) + ' грн.'
+        Li12.lastElementChild.textContent = (totalR / 1.2).toFixed(2) + ' грн.'
+        Li13.lastElementChild.textContent = ((totalR / 1.2) * 1.05).toFixed(2) + ' грн.'
+        Li14.lastElementChild.textContent = ((totalR / 1.2) * 1.1).toFixed(2) + ' грн.'
+        Li15.lastElementChild.textContent = ((totalR / 1.2) * 1.15).toFixed(2) + ' грн.'
+        Li16.lastElementChild.textContent = ((totalR / 1.2) * 1.2).toFixed(2) + ' грн.'
+        Li17.lastElementChild.textContent = ((totalR / 1.2) * 1.25).toFixed(2) + ' грн.'
+        Li18.lastElementChild.textContent = ((totalR / 1.2) * 1.3).toFixed(2) + ' грн.'
         break
       case "BTC":
         let rateB = data.find(elem => elem.ccy === "BTC")
         let totalB = Number(inputForm.value) * Number(rateB.sale)
-        Li11.firstElementChild.textContent = totalB.toFixed(2) + ' грн.'
-        Li12.firstElementChild.textContent = (totalB / 1.2).toFixed(2) + ' грн.'
-        Li13.firstElementChild.textContent = ((totalB / 1.2) * 1.05).toFixed(2) + ' грн.'
-        Li14.firstElementChild.textContent = ((totalB / 1.2) * 1.1).toFixed(2) + ' грн.'
-        Li15.firstElementChild.textContent = ((totalB / 1.2) * 1.15).toFixed(2) + ' грн.'
-        Li16.firstElementChild.textContent = ((totalB / 1.2) * 1.2).toFixed(2) + ' грн.'
-        Li17.firstElementChild.textContent = ((totalB / 1.2) * 1.25).toFixed(2) + ' грн.'
-        Li18.firstElementChild.textContent = ((totalB / 1.2) * 1.3).toFixed(2) + ' грн.'
+        Li11.lastElementChild.textContent = totalB.toFixed(2) + ' грн.'
+        Li12.lastElementChild.textContent = (totalB / 1.2).toFixed(2) + ' грн.'
+        Li13.lastElementChild.textContent = ((totalB / 1.2) * 1.05).toFixed(2) + ' грн.'
+        Li14.lastElementChild.textContent = ((totalB / 1.2) * 1.1).toFixed(2) + ' грн.'
+        Li15.lastElementChild.textContent = ((totalB / 1.2) * 1.15).toFixed(2) + ' грн.'
+        Li16.lastElementChild.textContent = ((totalB / 1.2) * 1.2).toFixed(2) + ' грн.'
+        Li17.lastElementChild.textContent = ((totalB / 1.2) * 1.25).toFixed(2) + ' грн.'
+        Li18.lastElementChild.textContent = ((totalB / 1.2) * 1.3).toFixed(2) + ' грн.'
         break
       default:
         return
